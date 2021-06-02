ebd96331c489c0296939b54bbb776ba79ba28637
Tomohito Yamazaki <t07840ty@gmail.com>
2021-06-02 11:47:00 +0900

    "Ubuntu version"

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,5 @@ cp -p .vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
-# zshの設定
-cp -p .zshrc ~/.zshrc
-chsh $USER -s $(which zsh)
-
 # gitの設定
 cp -p .gitconfig ~/.gitconfig
